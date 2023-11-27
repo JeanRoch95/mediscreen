40df23107a4a290fb6ed52a088d8b8bca9bcd326
Jean-Roch <82611021+JeanRoch95@users.noreply.github.com>
2023-11-27 14:54:33 +0100

    Creation base de donnée + fichier data patient - Sprint1

---
+++ microservice-patient/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO patients (firstname, lastname, dateofbirth, gender, address, phonenumber) VALUES
+                                                                                               ('John', 'Doe', '1980-01-01', 'M', '123 Main St, Anytown', '123-456-7890'),
+                                                                                               ('Jane', 'Smith', '1990-02-02', 'F', '456 Elm St, Anycity', '234-567-8901'),
+                                                                                               ('Alice', 'Johnson', '1985-03-03', 'F', '789 Oak St, Anystate', '345-678-9012'),
+                                                                                               ('Bob', 'Brown', '1975-04-04', 'M', '321 Maple St, Anycounty', '456-789-0123');
