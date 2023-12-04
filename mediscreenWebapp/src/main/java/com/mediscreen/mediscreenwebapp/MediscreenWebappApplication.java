1b2027b58f4ba06199e72d509e8a24bf58b135ce
Jean-Roch <82611021+JeanRoch95@users.noreply.github.com>
2023-12-04 13:28:56 +0100

    Ajout WebApp - SPRINT1

---
+++ mediscreenWebapp/src/main/java/com/mediscreen/mediscreenwebapp/MediscreenWebappApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen.mediscreenwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediscreenWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediscreenWebappApplication.class, args);
+	}
+
+}
