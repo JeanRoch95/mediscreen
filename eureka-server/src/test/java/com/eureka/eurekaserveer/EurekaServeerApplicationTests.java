15943d5f9ee01c4c63f678a544f32fc232a128c6
Jean-Roch <82611021+JeanRoch95@users.noreply.github.com>
2023-12-08 11:52:19 +0100

    Creation du serveur eureka

---
+++ eureka-server/src/test/java/com/eureka/eurekaserveer/EurekaServeerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.eurekaserveer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
