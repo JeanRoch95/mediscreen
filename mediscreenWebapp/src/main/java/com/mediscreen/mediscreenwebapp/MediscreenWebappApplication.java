1f878e5bbb20ead1f2d6f7ffb1eeb59a1057145a
Jean-Roch <82611021+JeanRoch95@users.noreply.github.com>
2023-12-09 15:03:38 +0100

    Ajout de webApp au config - SPRINT1

--- mediscreenWebapp/src/main/java/com/mediscreen/mediscreenwebapp/MediscreenWebappApplication.java
+++ mediscreenWebapp/src/main/java/com/mediscreen/mediscreenwebapp/MediscreenWebappApplication.java
@@ -2,12 +2,14 @@ package com.mediscreen.mediscreenwebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.mediscreen")
 @EnableDiscoveryClient
+@EnableConfigurationProperties
 public class MediscreenWebappApplication {
 
 	public static void main(String[] args) {
