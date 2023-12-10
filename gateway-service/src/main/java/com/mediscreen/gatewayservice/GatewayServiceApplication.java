4b32696dc457ccaf7ea617ee14b6e7936701be99
Jean-Roch <82611021+JeanRoch95@users.noreply.github.com>
2023-12-10 09:55:38 +0100

    Ajout du gateway-service au config repo -SPRINT 1

--- gateway-service/src/main/java/com/mediscreen/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/mediscreen/gatewayservice/GatewayServiceApplication.java
@@ -2,8 +2,10 @@ package com.mediscreen.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
