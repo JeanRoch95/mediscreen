fd009e1e5e9534fe434ba4622891a082a7dc5b66
Jean-Roch <82611021+JeanRoch95@users.noreply.github.com>
2023-12-10 10:10:35 +0100

    Configuration des routes du gateway et ajout du MS a eureka

--- gateway-service/src/main/java/com/mediscreen/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/mediscreen/gatewayservice/GatewayServiceApplication.java
@@ -3,9 +3,11 @@ package com.mediscreen.gatewayservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableConfigurationProperties
+@EnableDiscoveryClient
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
