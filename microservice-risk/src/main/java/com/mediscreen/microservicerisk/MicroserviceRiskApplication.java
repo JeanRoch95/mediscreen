4cf4f3a458aadf045e6cb1afa9a7d9365b5eeffe
Jean-Roch <82611021+JeanRoch95@users.noreply.github.com>
2023-12-17 11:34:27 +0100

    Creation du microservice-risk - SPRINT3✨

---
+++ microservice-risk/src/main/java/com/mediscreen/microservicerisk/MicroserviceRiskApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen.microservicerisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceRiskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceRiskApplication.class, args);
+	}
+
+}
---
+++ microservice-risk/src/test/java/com/mediscreen/microservicerisk/MicroserviceRiskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.microservicerisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceRiskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
